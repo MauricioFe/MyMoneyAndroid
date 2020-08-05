b1e6c2086c6107dba88c2555404fa7b07e757391
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-08-05 14:45:15 -0300

    Refatoração de código

--- app/src/main/java/mauriciofe/github/mymoney/ui/activities/fragments/relatorios/RelatoriosFragment.java
+++ app/src/main/java/mauriciofe/github/mymoney/ui/activities/fragments/relatorios/RelatoriosFragment.java
@@ -1,24 +1,35 @@
 package mauriciofe.github.mymoney.ui.activities.fragments.relatorios;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import mauriciofe.github.mymoney.R;
 
 
 public class RelatoriosFragment extends Fragment {
 
     private RelatoriosViewModel relatoriosViewModel;
 
-//    public View onCreateView(@NonNull LayoutInflater inflater,
-//                             ViewGroup container, Bundle savedInstanceState) {
-//        slideshowViewModel =
-//                ViewModelProviders.of(this).get(SlideshowViewModel.class);
-//        View root = inflater.inflate(R.layout.fragment_relatorios, container, false);
-//        final TextView textView = root.findViewById(R.id.text_slideshow);
-//        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
-//        return root;
-//    }
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        relatoriosViewModel =
+                ViewModelProviders.of(this).get(RelatoriosViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_relatorios, container, false);
+        final TextView textView = root.findViewById(R.id.text_slideshow);
+        relatoriosViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
 }
