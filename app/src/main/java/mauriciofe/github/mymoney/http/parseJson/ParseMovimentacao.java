63d3e71312404efcf05f07f8e83e2259d20b6186
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-10 16:13:31 -0300

    Método para parsear um objeto em json

--- app/src/main/java/mauriciofe/github/mymoney/http/parseJson/ParseMovimentacao.java
+++ app/src/main/java/mauriciofe/github/mymoney/http/parseJson/ParseMovimentacao.java
@@ -1,9 +1,27 @@
 package mauriciofe.github.mymoney.http.parseJson;
 
+import org.json.JSONException;
+import org.json.JSONStringer;
+
 import mauriciofe.github.mymoney.models.Movimentacoes;
 
 public class ParseMovimentacao {
     public static String parseForJson(Movimentacoes movimentacoes) {
-        return null;
+        JSONStringer js = new JSONStringer();
+        try {
+            js.object();
+            js.key("descricao").value(movimentacoes.getDescricao());
+            js.key("valor").value(movimentacoes.getValor());
+            js.key("date").value(movimentacoes.getData());
+            js.key("observacoes").value(movimentacoes.getObservacoes());
+            js.key("categoria_id").value(movimentacoes.getCategoria_id());
+            js.key("tipoMovimentacao_id").value(movimentacoes.getTipoMovimentacao_id());
+            js.key("repeticao_id").value(movimentacoes.getRepeticao_id());
+            js.key("usuario_id").value(movimentacoes.getUsuario_id());
+            js.endObject();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return js.toString();
     }
 }
