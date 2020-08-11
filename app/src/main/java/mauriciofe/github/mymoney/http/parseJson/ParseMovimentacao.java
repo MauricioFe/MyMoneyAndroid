791e6bc79c89b421d3cf9d16839788d9b9dd38f9
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-11 11:50:05 -0300

    Implementação do método para parsear dados em uma lista de moviemtações

--- app/src/main/java/mauriciofe/github/mymoney/http/parseJson/ParseMovimentacao.java
+++ app/src/main/java/mauriciofe/github/mymoney/http/parseJson/ParseMovimentacao.java
@@ -1,8 +1,12 @@
 package mauriciofe.github.mymoney.http.parseJson;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONStringer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import mauriciofe.github.mymoney.models.Movimentacoes;
 
@@ -40,6 +44,31 @@ public class ParseMovimentacao {
             movimentacoes.setRepeticao_id(jsonObject.getInt("repeticao_id"));
             movimentacoes.setUsuario_id(jsonObject.getInt("usuario_id"));
             return movimentacoes;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static List<Movimentacoes> parseForList(String content) {
+        List<Movimentacoes> movimentacoesList = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(content);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                Movimentacoes movimentacoes = new Movimentacoes();
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                movimentacoes.setId(jsonObject.getInt("id"));
+                movimentacoes.setDescricao(jsonObject.getString("descricao"));
+                movimentacoes.setValor(jsonObject.getDouble("valor"));
+                movimentacoes.setData(jsonObject.getString("data"));
+                movimentacoes.setObservacoes(jsonObject.getString("observacoes"));
+                movimentacoes.setCategoria_id(jsonObject.getInt("categoria_id"));
+                movimentacoes.setTipoMovimentacao_id(jsonObject.getInt("tipoMovimentacao_id"));
+                movimentacoes.setRepeticao_id(jsonObject.getInt("repeticao_id"));
+                movimentacoes.setUsuario_id(jsonObject.getInt("usuario_id"));
+                movimentacoesList.add(movimentacoes);
+            }
+            return movimentacoesList;
         } catch (JSONException e) {
             e.printStackTrace();
             return null;
--- app/src/main/java/mauriciofe/github/mymoney/tasks/movimentacao/GetMovimentacoes.java
+++ app/src/main/java/mauriciofe/github/mymoney/tasks/movimentacao/GetMovimentacoes.java
@@ -4,19 +4,28 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.ListView;
 
+import java.util.List;
+
 import mauriciofe.github.mymoney.http.conexao.HttpConnectionMovimentacoes;
+import mauriciofe.github.mymoney.http.parseJson.ParseMovimentacao;
+import mauriciofe.github.mymoney.models.Movimentacoes;
 
 public class GetMovimentacoes extends AsyncTask<String, String, String> {
     Context context;
-    ListView movimentacoesList;
+    ListView movimentacaoList;
 
     public GetMovimentacoes(Context context, ListView movimentacoesList) {
         this.context = context;
-        this.movimentacoesList = movimentacoesList;
+        this.movimentacaoList = movimentacoesList;
     }
 
     @Override
     protected String doInBackground(String... params) {
         return HttpConnectionMovimentacoes.getDados(params[0], params[1]);
+    }
+
+    @Override
+    protected void onPostExecute(String content) {
+        List<Movimentacoes> movimentacoesList = ParseMovimentacao.parseForList(content);
     }
 }
